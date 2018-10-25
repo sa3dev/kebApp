ee5499d1fbdeb44174e6d1f7d69ec13851e343b0
FormationJavaAngular <sa33dev@gmail.com>
2018-10-25 11:49:56 +0200

    debut de login

--- kebApp/src/app/app.module.ts
+++ kebApp/src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppComponent } from './app.component';
 import {CoreModule} from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
- 
-  ],
-  imports: [
-    BrowserModule,
-    CoreModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+
+	],
+	imports: [
+		BrowserModule,
+		CoreModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
