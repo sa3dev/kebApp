1583f9dd7b15c8b6789152319d10aa43ab53b7fb
JoseStall <g.delory62@gmail.com>
2018-10-25 13:35:00 +0200

    modif register

--- kebApp/src/app/core/users/register/register.component.ts
+++ kebApp/src/app/core/users/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RegisterService } from './register.service';
+import { User } from '../user.model'
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  public firstNameCtrl: FormControl;
+  public lastNameCtrl: FormControl;
+  public nickNameCtrl: FormControl;
+  public emailCtrl: FormControl;
+  public passwordCtrl: FormControl;
+  public passwordCheckCtrl: FormControl;
+  public loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private registerService: RegisterService, ) { }
 
   ngOnInit() {
+    this.firstNameCtrl = this.fb.control('', Validators.required);
+    this.lastNameCtrl = this.fb.control('', Validators.required);
+    this.nickNameCtrl = this.fb.control('', Validators.required);
+    this.emailCtrl = this.fb.control('', Validators.required);
+    this.passwordCtrl = this.fb.control('', Validators.required);
+    this.passwordCheckCtrl = this.fb.control('', Validators.required);
+    this.loginForm = this.fb.group({
+      firstName: this.firstNameCtrl,
+      lastName: this.lastNameCtrl,
+      nickName: this.nickNameCtrl,
+      email: this.emailCtrl,
+      password: this.passwordCtrl,
+      passwordCheck: this.passwordCheckCtrl
+    });
   }
-
+  signup() {
+    if (this.passwordCtrl.value === this.passwordCheckCtrl.value) {
+      const user = new User();
+      user.first_name = this.firstNameCtrl.value;
+      user.last_name = this.lastNameCtrl.value;
+      user.nick_name = this.nickNameCtrl.value;
+      user.email = this.emailCtrl.value;
+      user.password = this.passwordCtrl.value;
+      this.registerService.createUser(user);
+    }
+  }
 }
+
+
--- kebApp/src/app/core/users/user.model.ts
+++ kebApp/src/app/core/users/user.model.ts
@@ -1,2 +1,9 @@
-export class UserModel {
+export class User {
+    id: number;
+    first_name: string;
+    last_name: string;
+    nick_name: string;
+    email: string;
+    password: string;
+    token: string;
 }
