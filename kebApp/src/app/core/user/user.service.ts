5534c19773f40642de0f4b9761671dd481cacb08
FormationJavaAngular <sa33dev@gmail.com>
2018-10-25 14:24:53 +0200

    recuperation source L

--- kebApp/src/app/core/user/user.service.ts
+++ kebApp/src/app/core/user/user.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient , HttpParams  } from '@angular/common/http';
+import { User } from './user.model';
+import { Router } from '@angular/router';
 
-
+const urlUser = "http://127.0.0.1:3000/users";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class UserService {
 
+	private isIdentifier = false;
 
-
-	constructor() { }
+	constructor(
+		private httpClient: HttpClient,
+		private router: Router
+	) { }
 
 	/**
-	 * 
+	 * Said
 	 * @param param1 first param of thue user, it can be firstname or email
 	 * @param password 
 	 * 
@@ -23,7 +28,26 @@ export class UserService {
 
 		const service = this;
 
-		//const param = param1 ? {  };
-		return
+		// we set a param if we found params for first_name or email  
+		const params = param1 ? { params : new HttpParams().set( 'first_name' || 'email' , param1 ).set( 'password' , password ) } : { };
+		
+		const obser = this.httpClient.get<User[]>( urlUser, params ).subscribe( (data) => { 
+			if( data.length === 1 ){
+
+				service.isIdentifier = true
+			}else{
+				service.isIdentifier = false
+			}
+			
+			//this.router.navigate(['./admin']);
+		},
+			error => console.log(error)
+		);
+	}
+	/**
+	 * 
+	 */
+	canActivate(): boolean{
+		return this.isIdentifier;
 	}
 }
