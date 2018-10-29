b6650a613611ea3239c669e8988d969de4beceba
apolineaire <pierreemmanuel.denys@gmail.com>
2018-10-29 11:19:48 +0100

    add working updateProduct method

--- kebApp/src/app/core/products/products.service.ts
+++ kebApp/src/app/core/products/products.service.ts
@@ -7,8 +7,7 @@ import { apiURLProducts } from "../../config";
   providedIn: 'root'
 })
 export class ProductsService {
-
-  newproperty:string;
+  productUpdated: any;
   constructor(private http: HttpClient) { }
   /**
    * Function to get the list of products that returns an Observable of Product[]
@@ -32,17 +31,23 @@ export class ProductsService {
    * @param value new value for the property defined in the second argument
    * 
    */
-  updateProduct(id: number, property:string, value) {
-    this.newproperty = property
-    this.http.patch(apiURLProducts + id, {
-      newproperty: value,
-    }).subscribe(data => console.log(data), error => console.log("Error in the updateProduct method " + error))
+  updateProduct(id: number, property: string, value) {
+    // The road to the object
+    const apiUrlId = `${apiURLProducts}/${id}`;
+    // 1. We get the object (though there should be a way to get the object already used in our ngFor without calling again the API) 2. we copy it and update it productUpdated[property] = value 3. we put the updated object at the same place in apiUrlId 
+    this.http.get<Product>(apiUrlId).subscribe(data => {
+      this.productUpdated = data;
+      this.productUpdated[property] = value;
+      console.log(this.productUpdated);
+      this.http.put(apiUrlId, this.productUpdated).subscribe(data => console.log(data), error => console.log(error))
+    })
   }
+  
   /**
    * A function to add a product to our list of products
    * @param product the product of type Product that you want to add to the list of products
    */
-  addProduct(product:Product){
+  addProduct(product: Product) {
     this.http.post(apiURLProducts, product);
   }
 }
