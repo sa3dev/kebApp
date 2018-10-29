67fb1529e4737320ee704bcf555d254bbd1a8638
riku59250 <leon.demeyere@hotmail.fr>
2018-10-29 09:04:11 +0100

    final branche 1

--- kebApp/src/app/core/user/user.service.ts
+++ kebApp/src/app/core/user/user.service.ts
@@ -1,87 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient , HttpParams  } from '@angular/common/http';
 import { User } from './user.model';
-<<<<<<< HEAD
 import {Observable, forkJoin} from 'rxjs';
 import { tap, map, concat } from 'rxjs/operators';
 
 
 const urlUser= "http://127.0.0.1:3000/users";
-=======
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-
-const urlUser = "http://127.0.0.1:3000/users";
-
->>>>>>> 4ebda1bc7c886ace5d2d62c2bec4c2af37c9fbc6
 @Injectable({
 	providedIn: 'root'
 })
 export class UserService {
 	private  tabUser:User[]=[];
 
-<<<<<<< HEAD
 	
 
 	constructor(private httpclient:HttpClient) { }
-=======
-	private isIdentifier = false;
-
-	constructor(
-		private httpClient: HttpClient,
-		private router: Router
-	) { }
->>>>>>> 4ebda1bc7c886ace5d2d62c2bec4c2af37c9fbc6
 
 	/**
-	 * Sa
+	 * 
 	 * @param param1 first param of thue user, it can be firstname or email
 	 * @param password 
 	 * 
 	 * log the user and redirect him in the application
-	 * return redirection to the page admin 
 	 */
 	searchUser( param1: string , password: string ){
+
 		const service = this;
 
-		// we set a param if we found params for first_name or email  
-		const params = param1 ? { params : new HttpParams().set( 'first_name' || 'email' , param1 ).set( 'password' , password ) } : { };
-		
-		const obser = this.httpClient.get<User[]>( urlUser, params ).subscribe( (data) => { 
-
-			if( data.length === 1 ){
-
-				service.isIdentifier = true
-			}else{
-				service.isIdentifier = false
-			}
-			// console.log('Redirection en cours ..');
-			// console.log(service.isIdentifier);
-			
-			this.router.navigate(['./admin']);	
-		},
-			error => console.log(error)
-		);
-	}
-
-	/**
-	 *	Set the the boolean to let passe the user/admin and let use the correspondant service 
-	 */
-	canActivate(): boolean{
-		return this.isIdentifier;
-	}
-
-	getAllUsers(): Observable<User[]> {
-		return this.httpClient.get<User[]>(urlUser);
-	}
-
-	delete( id: number ){
-
-		let url = `${urlUser}/${id}`;
-
-		this.httpClient.delete(url).subscribe(
-			
-		);
+		//const param = param1 ? {  };
+		return
 	}
 	/**
 	 * Léon
@@ -150,14 +97,7 @@ export class UserService {
 			this.getEmail(email),
 			this.getIdentifiant(identifiant)
 		).pipe(
-			map(([usersEmail, usersIdentifiant])=>usersEmail=usersEmail.concat(usersIdentifiant)
-				
-			
-				
-				
-				
-
-			)
+			map(([usersEmail, usersIdentifiant])=>usersEmail=usersEmail.concat(usersIdentifiant))
 		)
 	}
 
