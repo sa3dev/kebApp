a2a56a8c4637de8642d2e38c5b2bf844bd63ac3e
riku59250 <leon.demeyere@hotmail.fr>
2018-11-08 14:13:12 +0100

    tesst service fournisseur

--- kebApp/src/app/fournisseur/fournisseur.service.spec.ts
+++ kebApp/src/app/fournisseur/fournisseur.service.spec.ts
@@ -1,12 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed,async } from '@angular/core/testing';
 
 import { FournisseurService } from './fournisseur.service';
-
+import { HttpClientModule } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { Fournisseur } from './fournisseur.model';
 describe('FournisseurService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports:[
+      HttpClientModule
+    ]
+  }));
 
-  it('should be created', () => {
+  it('should be created', async( () => {
+  
     const service: FournisseurService = TestBed.get(FournisseurService);
+    
     expect(service).toBeTruthy();
-  });
+    expect(service.getFournisseur() instanceof BehaviorSubject).toBe(true);
+    expect(service.getFournisseur().value instanceof Array ).toBe(true);
+    
+    })); 
+  
 });
